9cb3c14b7158ed2da50aa0f4a14a8814e52e0139
cac03 <happy96538@gmail.com>
2016-10-05 20:47:14 +0300

    Initial commit

---
+++ app/src/main/java/com/caco3/testtask/songs/SongsActivity.java
@@ -0,0 +1,15 @@
+package com.caco3.testtask.songs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.caco3.testtask.R;
+
+public class SongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songs);
+    }
+}
---
+++ app/src/main/java/com/caco3/testtask/util/LogUtils.java
@@ -0,0 +1,85 @@
+package com.caco3.testtask.util;
+
+import android.util.Log;
+
+
+
+public class LogUtils {
+    private static final int MAX_LOG_TAG_LENGTH = 23;
+
+    private static final boolean LOGGING_ENABLED = true;
+
+    public static String makeLogTag(String str){
+        if (str.length() > MAX_LOG_TAG_LENGTH){
+            return str.substring(0, MAX_LOG_TAG_LENGTH);
+        } else {
+            return str;
+        }
+    }
+
+    public static String makeLogTag(Class clazz){
+        return makeLogTag(clazz.getSimpleName());
+    }
+
+    public static void LOGE(String tag, String msg, Throwable cause){
+        if (LOGGING_ENABLED){
+            Log.e(tag, msg, cause);
+        }
+    }
+
+    public static void LOGE(String tag, String msg){
+        if (LOGGING_ENABLED){
+            Log.e(tag, msg);
+        }
+    }
+
+    public static void LOGD(String tag, String msg, Throwable cause){
+        if (LOGGING_ENABLED){
+            Log.d(tag, msg, cause);
+        }
+    }
+
+    private static void LOGD(String tag, String msg){
+        if (LOGGING_ENABLED){
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void LOGI(String tag, String msg, Throwable cause){
+        if (LOGGING_ENABLED){
+            Log.i(tag, msg, cause);
+        }
+    }
+
+    public static void LOGI(String tag, String msg){
+        if (LOGGING_ENABLED){
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void LOGW(String tag, String msg, Throwable cause){
+        if (LOGGING_ENABLED){
+            Log.w(tag, msg, cause);
+        }
+    }
+
+    public static void LOGW(String tag, String msg){
+        if (LOGGING_ENABLED){
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void LOGV(String tag, String msg, Throwable cause){
+        if (LOGGING_ENABLED){
+            Log.v(tag, msg, cause);
+        }
+    }
+
+    public static void LOGV(String tag, String msg){
+        if (LOGGING_ENABLED){
+            Log.v(tag, msg);
+        }
+    }
+
+
+}
