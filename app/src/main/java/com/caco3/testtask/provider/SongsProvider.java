0ffdc87b00286f2a110a0366560e0a2c2f0ad886
cac03 <happy96538@gmail.com>
2016-10-07 16:49:14 +0300

    Throw IllegalArgumentException if unknown uri provided in SongsProvider for all crud operations

--- app/src/main/java/com/caco3/testtask/provider/SongsProvider.java
+++ app/src/main/java/com/caco3/testtask/provider/SongsProvider.java
@@ -9,10 +9,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
+import java.util.Arrays;
+
 import static com.caco3.testtask.util.LogUtils.*;
 
 public class SongsProvider extends ContentProvider {
     private static final String TAG = makeLogTag(SongsProvider.class);
+
+    private static final String SONG_TYPE = "com.caco3.testtask.datatype.song";
 
     private static final String AUTHORITY = "com.caco3.testtask.songs";
 
@@ -41,7 +45,11 @@ public class SongsProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        LOGI(TAG, "query: " + uri);
+        LOGD(TAG, "Query: " + uri
+                + " projection: " + Arrays.toString(projection)
+                + " selection: " + selection
+                + " args: " + Arrays.toString(selectionArgs)
+                + " sortOrder: " + sortOrder);
         SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
         queryBuilder.setTables(SongsDatabase.Songs.TABLE_NAME);
         switch (sUriMatcher.match(uri)){
@@ -64,7 +72,7 @@ public class SongsProvider extends ContentProvider {
     public String getType(Uri uri) {
         switch (sUriMatcher.match(uri)){
             case URI_SONGS:
-                return "songs";
+                return SONG_TYPE;
             default:
                 throw new IllegalArgumentException("Unknown uri: " + uri);
         }
@@ -74,19 +82,47 @@ public class SongsProvider extends ContentProvider {
     public Uri insert(Uri uri, ContentValues values) {
         LOGI(TAG, "insert: " + uri + " cv = " + values);
         mSQLiteDatabase = mSongsDatabase.getWritableDatabase();
-        long rowId = mSQLiteDatabase.insert(SongsDatabase.Songs.TABLE_NAME, null, values);
+        long rowId;
+        switch(sUriMatcher.match(uri)){
+            case URI_SONGS:
+                rowId = mSQLiteDatabase.insert(SongsDatabase.Songs.TABLE_NAME, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown uri: " + uri);
+        }
         return ContentUris.withAppendedId(uri, rowId);
+
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
+        LOGD(TAG, "Delete: " + uri + " selection: " + selection + " args: " + Arrays.toString(selectionArgs));
         mSQLiteDatabase = mSongsDatabase.getWritableDatabase();
-        return mSQLiteDatabase.delete(SongsDatabase.Songs.TABLE_NAME, selection, selectionArgs);
+        int affectedRows;
+        switch (sUriMatcher.match(uri)){
+            case URI_SONGS:
+                affectedRows = mSQLiteDatabase.delete(SongsDatabase.Songs.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown uri: " + uri);
+        }
+
+        return affectedRows;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        LOGD(TAG, "Update: " + uri + " selection: " + selection + " args: " + Arrays.toString(selectionArgs));
         mSQLiteDatabase = mSongsDatabase.getWritableDatabase();
-        return mSQLiteDatabase.update(SongsDatabase.Songs.TABLE_NAME, values, selection, selectionArgs);
+        int affectedRows;
+        switch (sUriMatcher.match(uri)){
+            case URI_SONGS:
+                affectedRows = mSQLiteDatabase.update(SongsDatabase.Songs.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown uri: " + uri);
+        }
+
+        return affectedRows;
     }
 }
